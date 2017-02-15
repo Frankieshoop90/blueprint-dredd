c868cf56bcf85a7406278efdfba6745816dc98d7
Т Гончар < >
2017-02-15 17:28:45 +0200

    GTG | API-1832

--- hooks/hook-me.js
+++ hooks/hook-me.js
@@ -40,7 +40,7 @@ hooks.afterAll(function (transactions, done) {
     hooks.log('after all');
     done();
 })*/
-
+/*--------------------------------------application------------------------------------------------------*/
 var appID;
 
 hooks.after("Apllication collection > Creates a new application", function (transaction, done) {
@@ -73,8 +73,128 @@ hooks.before("Apllication collection > Deletes application item", function (tran
     done();
 });
 
+/*--------------------------------------templates------------------------------------------------------*/
 
-hooks.afterAll(function (transactions, done) {
-    hooks.log(transactions.fullPath);
+var templID;
+var hashID;
+
+hooks.after("Template collection > Creates a new document template by uploading file from URL", function (transaction, done) {
+
+    templID = JSON.parse(transaction.real.body)['id'];
+    hooks.log('Successfully created template_id => '+templID);
+    done();
+});
+hooks.before("Template collection > Information about created template", function (transaction, done) {
+    //reusing data from previous response here
+    //replacing id in URL with stashed id from previous response
+    var url = transaction.fullPath;
+    transaction.fullPath = url.replace('1023456789', templID);
+    hooks.log('Successfully removed {template_id} and replaced by current template_id => '+templID);
+    done();
+});
+hooks.before("Template collection > Updates template information", function (transaction, done) {
+    //reusing data from previous response here
+    //replacing id in URL with stashed id from previous response
+    var url = transaction.fullPath;
+    transaction.fullPath = url.replace('1023456789', templID);
+    hooks.log('Successfully removed {template_id} and replaced by current template_id => '+templID);
+    done();
+});
+hooks.before("Template collection > Fillable fields for selected document template", function (transaction, done) {
+    //reusing data from previous response here
+    //replacing id in URL with stashed id from previous response
+    var url = transaction.fullPath;
+    transaction.fullPath = url.replace('1023456789', templID);
+    hooks.log('Successfully removed {template_id} and replaced by current template_id => '+templID);
+    done();
+});
+hooks.before("Template collection > Downloads document template", function (transaction, done) {
+    transaction.skip = true;
+    hooks.log('skipped');
+    done();
+});
+hooks.before("Template collection > Child documents for selected document template", function (transaction, done) {
+    //reusing data from previous response here
+    //replacing id in URL with stashed id from previous response
+    var url = transaction.fullPath;
+    transaction.fullPath = url.replace('1023456789', 20748422);
+    hooks.log('Successfully removed {template_id} and replaced by current template_id => '+templID);
+    done();
+});
+hooks.before("Template collection > Downloads zip archive of signatures", function (transaction, done) {
+    //reusing data from previous response here
+    //replacing id in URL with stashed id from previous response
+    transaction.skip = true;
+  /*  var url = transaction.fullPath;
+    transaction.fullPath = url.replace('1023456789', templID);
+    hooks.log('Successfully removed {template_id} and replaced by current template_id => '+templID);*/
+    done();
+});
+hooks.before("Template collection > Fills a fillable form template", function (transaction, done) {
+    //reusing data from previous response here
+    //replacing id in URL with stashed id from previous response
+    var url = transaction.fullPath;
+    transaction.fullPath = url.replace('1023456789', templID);
+    hooks.log('Successfully removed {template_id} and replaced by current template_id => '+templID);
+    done();
+});
+hooks.before("Template collection > Uploading the initial version of the document", function (transaction, done) {
+    //reusing data from previous response here
+    //replacing id in URL with stashed id from previous response
+    transaction.skip=true;
+    done();
+});
+hooks.before("Template collection > Get values for fillable form", function (transaction, done) {
+    //reusing data from previous response here
+    //replacing id in URL with stashed id from previous response
+    var url = transaction.fullPath;
+    transaction.fullPath = url.replace('1023456789', templID);
+    hooks.log('Successfully removed {template_id} and replaced by current template_id => '+templID);
+    done();
+});
+hooks.before("Template collection > Creates a new share link to document", function (transaction, done) {
+    //reusing data from previous response here
+    //replacing id in URL with stashed id from previous response
+    var url = transaction.fullPath;
+    transaction.fullPath = url.replace('1023456789', templID);
+    hooks.log('Successfully removed {template_id} and replaced by current template_id => '+templID);
+    done();
+});
+hooks.after("Template collection > Creates a new share link to document", function (transaction, done) {
+    hashID = JSON.parse(transaction.real.body)['hash'];
+    hooks.log(hashID);
+    done();
+});
+hooks.before("Template collection > Retrieve a list of hash", function (transaction, done) {
+    //reusing data from previous response here
+    //replacing id in URL with stashed id from previous response
+    var url = transaction.fullPath;
+    transaction.fullPath = url.replace('1023456789', templID);
+    hooks.log('Successfully removed {template_id} and replaced by current template_id => '+templID);
+    done();
+});
+hooks.before("Template collection > Deletes one share link to document by hash", function (transaction, done) {
+    //reusing data from previous response here
+    //replacing id in URL with stashed id from previous response
+    transaction.fullPath = transaction.fullPath.replace('1023456789', templID);
+    transaction.fullPath = transaction.fullPath.replace('hash', hashID);
+    hooks.log('Successfully removed {template_id} and replaced by current template_id => '+templID);
+    hooks.log('Successfully removed {hash} and replaced by current hash => '+hashID);
+    done();
+});
+hooks.before("Template collection > Deletes all share links to document by id", function (transaction, done) {
+    //reusing data from previous response here
+    //replacing id in URL with stashed id from previous response
+    var url = transaction.fullPath;
+    transaction.fullPath = url.replace('1023456789', templID);
+    hooks.log('Successfully removed {template_id} and replaced by current template_id => '+templID);
+    done();
+});
+hooks.before("Template collection > Deletes document template item", function (transaction, done) {
+    //reusing data from previous response here
+    //replacing id in URL with stashed id from previous response
+    var url = transaction.fullPath;
+    transaction.fullPath = url.replace('1023456789', templID);
+    hooks.log('Successfully removed {template_id} and replaced by current template_id => '+templID);
     done();
 });
